9923ebb91fd997757082b8507197c3ac5bda9dea
Similoluwa Abidoye <similoluwaeyitayoabidoye@gmail.com>
2025-08-10 07:22:46 +0100

    feat: add the smartcontract and also initialize the configuration of the abi in the frontend

---
+++ src/app/abi/index.tsx
@@ -0,0 +1,6 @@
+import PrizePoolPredictionABI from "./PrizePoolPrediction-abi.json";
+
+export const PrizePredictionContract = {
+    address: "0x9b39Fb4c93d80dF3E91a0369c5B6599Cf80873A4",
+    abi: PrizePoolPredictionABI,
+}
